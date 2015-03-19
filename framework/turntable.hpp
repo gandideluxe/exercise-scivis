2327bc1198cab80311b33e8f77a175dc2ff542f6
Sebastian Thiele <sebastian.thiele@uni-weimar.de>
2015-03-19 17:44:54 +0100

    Fixed Turntable rotation

--- framework/turntable.hpp
+++ framework/turntable.hpp
@@ -36,11 +36,12 @@ public:
   }
 
   inline glm::mat4 matrix() const {
-    return glm::translate(glm::vec3(0.0f, 0.0f, -m_zoom))
-         * glm::translate(glm::vec3(m_panning.x, m_panning.y, 0.0f))
-         * glm::rotate(m_rotation.x, glm::vec3(0.0f,1.0f,0.0f))
-         * glm::rotate(-m_rotation.y, glm::vec3(1.0f,0.0f,0.0f))
-         ;
+      glm::vec4 rotate_x = glm::rotate(m_rotation.x, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(1.0, 0.0f, 0.0f, 1.0f);
+      return glm::translate(glm::vec3(0.0f, 0.0f, -m_zoom))
+          * glm::translate(glm::vec3(m_panning.x, m_panning.y, 0.0f))
+          * glm::rotate(m_rotation.x, glm::vec3(0.0f, 1.0f, 0.0f))
+          * glm::rotate(-m_rotation.y, glm::vec3(rotate_x.x, 0.0f, -rotate_x.z))
+          ;
   }
 
   float       zoomScale;
